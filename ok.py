fd28627fa4ce49814e3f84dd70e3218f74296dcd
georgeolivercx8-dot <georgeolivercx8@gmail.com>
2026-06-13 08:28:30 +0100

    Add files via upload

---
+++ ok.py
@@ -0,0 +1,22 @@
+import os
+import webview
+
+USERNAME = "NITRO"
+
+class Api:
+    def close_app(self):
+        webview.windows[0].destroy()
+
+file_path = os.path.abspath("profile.html")
+
+if not os.path.exists(file_path):
+    print("ERROR: profile.html not found next to ok.py")
+else:
+    print("Launching:", file_path)
+    webview.create_window(
+        USERNAME,
+        file_path,
+        fullscreen=True,
+        js_api=Api()
+    )
+    webview.start(debug=False)
